8c829bd0b9231b33e469a8d42c195773570efa33
Jean-Philippe Quéméner <jeanphilippe.quemener@gmail.com>
2015-11-10 20:01:45 +0100

    add lib module

--- ui/src/main/java/io/docker/ui/Main.java
+++ ui/src/main/java/io/docker/ui/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/simple_docker.fxml"));
-        primaryStage.setTitle("Simple Docker™ v0.0.1");
+        primaryStage.setTitle("Simple Docker™ v0.1.0");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.setResizable(true);
         primaryStage.show();
--- ui/src/main/java/io/docker/ui/RootController.java
+++ ui/src/main/java/io/docker/ui/RootController.java
@@ -1,10 +1,14 @@
 package io.docker.ui;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTabPane;
 import io.docking.core.order.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.Tab;
 
 /**
  * @author sascha
@@ -15,6 +19,12 @@ public class RootController {
     private ComboBox o1_prod1, o1_prod2, o1_prod3, o1_prod4, o1_prod5;
     private ComboBox[] comboBoxList;
 
+    @FXML
+    private JFXButton buttonOk;
+
+    @FXML
+    private JFXTabPane tabPane;
+
     public RootController() {
 
     }
@@ -22,7 +32,15 @@ public class RootController {
     @FXML
     public void initialize() {
         initComboBoxes();
+        initButtonOk();
     }
+
+
+    public void initButtonOk() {
+        SingleSelectionModel<Tab> selectionModel = tabPane.getSelectionModel();
+        buttonOk.setOnMouseClicked(e -> selectionModel.select(2));
+    }
+
 
     /**
      * initializes the order combo boxes
