0cf207974bf1d5134f6c1f74fb9a7efcc5e2df9b
Jean-Philippe Quéméner <jeanphilippe.quemener@gmail.com>
2015-12-01 15:28:44 +0100

    add javadocs

--- core/src/main/java/io/docking/core/Docker.java
+++ core/src/main/java/io/docking/core/Docker.java
@@ -5,6 +5,7 @@ import io.docking.core.order.Order;
 
 /**
  *
+<<<<<<< Updated upstream
  *
  */
 public interface Docker {
@@ -18,6 +19,24 @@ public interface Docker {
      * @return the {@link DockingPlan DockingPlan} to get on each wagon batch
      *         the matching order
      */
+=======
+ * Simple interface that provides a {@link DockingPlan DockingPlan} for the
+ * given {@link io.docking.core.order.Order orders} and
+ * {@link WagonBatch wagonbatchs}.
+ */
+public interface Docker {
+
+  /**
+   *
+   * @param orderOne - order from the first destination
+   * @param orderTwo - order from the second destination
+   * @param wagonBatchOne - delivery from the first location
+   * @param wagonBatchTwo - delivery from the second location
+	 *
+   * @return the {@link DockingPlan DockingPlan} to get on each wagonbatch
+   *         the matching order
+   */
+>>>>>>> Stashed changes
 	DockingPlan dock(Order orderOne, Order orderTwo, WagonBatch wagonBatchOne,
               WagonBatch wagonBatchTwo);
 
