b8a36887535916476d0d68e50518e81027be24cf
Jean-Philippe Quéméner <jeanphilippe.quemener@gmail.com>
2016-01-20 21:15:02 +0100

    refactor interface

--- core/src/main/java/io/docking/core/SimpleDocker.java
+++ core/src/main/java/io/docking/core/SimpleDocker.java
@@ -3,16 +3,17 @@ package io.docking.core;
 
 import io.docking.core.order.Order;
 
+import java.util.List;
+
 /**
  * 
- * @author sascha
+ * @author Jean-Philippe Quéméner
  *
  */
 public class SimpleDocker implements Docker {
 
 	@Override
-	public DockingPlan dock(Order orderOne, Order orderTwo,
-							WagonBatch wagonBatchOne, WagonBatch wagonBatchTwo) {
+	public DockingPlan dock(List<Order> orders, List<WagonBatch> trains) {
 		return null;
 	}
 }
