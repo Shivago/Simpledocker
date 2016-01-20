81eae4e15a3463c8be181527c8758c344f9966c6
Jean-Philippe Quéméner <jeanphilippe.quemener@gmail.com>
2016-01-20 20:49:34 +0100

    add menubar

--- ui/src/main/java/io/docker/ui/view/application/ApplicationView.java
+++ ui/src/main/java/io/docker/ui/view/application/ApplicationView.java
@@ -9,6 +9,10 @@ import io.docker.ui.view.order.OrderView;
 import io.docker.ui.view.summary.SummaryView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -34,6 +38,9 @@ public class ApplicationView implements FxmlView<ApplicationViewModel>,
     public JFXTabPane tabPane;
 
     @FXML
+    public MenuBar applicationMenubar;
+
+    @FXML
     // Inject the Code behind instance of the orderView by using the
     // nameconvention ...Controller
     private OrderView orderViewController;
@@ -56,8 +63,28 @@ public class ApplicationView implements FxmlView<ApplicationViewModel>,
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         tabService.setTabPane(tabPane);
+        buildMenu();
     }
 
+    private void buildMenu() {
+        final Menu fileMenu = new Menu("Vorgang");
+        final MenuItem loadItem = new MenuItem("laden..");
+        final MenuItem saveItem = new MenuItem("speichern/exportieren..");
+        final SeparatorMenuItem seperator = new SeparatorMenuItem();
+        final MenuItem exitItem = new MenuItem("Programm verlassen");
+        fileMenu.getItems().addAll(loadItem, saveItem, seperator, exitItem);
+        applicationMenubar.getMenus().add(fileMenu);
+
+        final Menu configMenu = new Menu("Eigenschaften");
+        final MenuItem configLoadItem = new MenuItem("Produktdatenbank laden..");
+        configMenu.getItems().addAll(configLoadItem);
+        applicationMenubar.getMenus().addAll(configMenu);
+
+    }
+
+    public void openConfiguration() {
+
+    }
 
 
 
